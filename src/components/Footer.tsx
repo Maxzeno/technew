0c97b6d686210b10b8c47a181f6bad77db32c7d4
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-06-27 12:40:28 +0100

    added link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./../custom.css";
 
 function Footer() {
@@ -11,21 +12,21 @@ function Footer() {
             <div className="text-[18px] font-medium leading-[24px]">Copyrights @ Progfams 2024</div>
           </div>
           <div className="col-span-1">
-            <a href="/" className="text-[20px] font-medium leading-[24px] hover:text-[#4393D8] block mb-7">Home</a>
-            <a href="/" className="text-[20px] font-medium leading-[24px] hover:text-[#4393D8] block mb-7">About Us</a>
-            <a href="/" className="text-[20px] font-medium leading-[24px] hover:text-[#4393D8] block">Service</a>
+            <Link to="#" className="text-[20px] font-medium leading-[24px] hover:text-[#4393D8] block mb-7">Home</Link>
+            <Link to="#" className="text-[20px] font-medium leading-[24px] hover:text-[#4393D8] block mb-7">About Us</Link>
+            <Link to="#" className="text-[20px] font-medium leading-[24px] hover:text-[#4393D8] block">Service</Link>
           </div>
           <div className="col-span-1">
-            <a href="/" className="text-[20px] font-medium leading-[24px] hover:text-[#4393D8] block mb-7">Works</a>
-            <a href="/" className="text-[20px] font-medium leading-[24px] hover:text-[#4393D8] block mb-7">Boot Camp</a>
+            <Link to="#" className="text-[20px] font-medium leading-[24px] hover:text-[#4393D8] block mb-7">Works</Link>
+            <Link to="#" className="text-[20px] font-medium leading-[24px] hover:text-[#4393D8] block mb-7">Boot Camp</Link>
           </div><div className="col-span-1">
-            <a href="/" className="text-[20px] font-medium leading-[24px] hover:text-[#4393D8] block mb-7">Contribution</a>
-            <a href="/" className="text-[20px] font-medium leading-[24px] hover:text-[#4393D8] block">Blog</a>
+            <Link to="#" className="text-[20px] font-medium leading-[24px] hover:text-[#4393D8] block mb-7">Contribution</Link>
+            <Link to="#" className="text-[20px] font-medium leading-[24px] hover:text-[#4393D8] block">Blog</Link>
           </div>
           <div>
             <div className="text-[20px] font-medium leading-[24px] block mb-7">Contact</div>
-            <a href="tel:+2348101128072" className="text-[16px] opacity-75 font-medium leading-[24px] mb-5 block">+234 810 112 8072</a>
-            <a href="mailto:info@progfams.com" className="text-[16px] font-medium leading-[24px] mb-5 block">Info@Progfams.com</a>
+            <Link to="tel:+2348101128072" className="text-[16px] opacity-75 font-medium leading-[24px] mb-5 block">+234 810 112 8072</Link>
+            <Link to="mailto:info@progfams.com" className="text-[16px] font-medium leading-[24px] mb-5 block">Info@Progfams.com</Link>
             <div className="text-[16px] font-medium leading-[24px] opacity-75 mb-5">10B, Harmony Close, Ikeja, Lagos.</div>
 
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
   let location = useLocation();
@@ -7,8 +7,8 @@ function Navbar() {
     <div className="flex justify-between items-center py-[42px] text-white">
         <div className="font-extrabold text-[28px] leading-[42px] flex-1 font-poppins">TECHNEW</div>
         <div className="font-medium text-[20px] leading-[24px] flex-1 flex justify-center font-montserrat">
-        <a href="#/" className={`mr-[50px] ${location.pathname === "/" ? "text-[#4393D8]" : "hover:text-[#4393D8]" }`}>Home</a>
-              <a href="#/todo" className={location.pathname === "/todo" ? "text-[#4393D8]" : "hover:text-[#4393D8]"}>Bootcamp</a>
+        <Link to="/" className={`mr-[50px] ${location.pathname === "/" ? "text-[#4393D8]" : "hover:text-[#4393D8]" }`}>Home</Link>
+        <Link to="/todo" className={location.pathname === "/todo" ? "text-[#4393D8]" : "hover:text-[#4393D8]"}>Bootcamp</Link>
         </div>
        <div className="flex-1"></div>
     </div>
